82442b806741c855af0d07c3156b5692c638ac5b
Touhidul Shawan <touhidulislamshaon@gmail.com>
2019-03-30 04:30:52 +0600

    solved

---
+++ Python Basic  Part I/Problem 1.py
@@ -0,0 +1,8 @@
+"""
+Here I can use only one print() function but for pep8 indentation I have used 3
+
+"""
+
+print('Twinkle, twinkle, little star,\n\tHow I wonder what you are!')
+print('\t\tUp above the world so high,\n\t\tLike a diamond in the sky.')
+print('Twinkle, twinkle, little star,\n\tHow I wonder what you are')
---
+++ Python Basic  Part I/Problem 2.py
@@ -0,0 +1,6 @@
+#  Write a Python program to get the Python version you are using.
+
+import sys
+
+my_python_version = sys.version
+print(my_python_version)
